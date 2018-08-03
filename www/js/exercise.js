09996ba9e693755b5c04a3e8dfcd8d22e91fefcf
cappuccinocat <xinglydia@gmail.com>
2018-08-03 16:02:49 -0700

    all the exercise types done!

--- www/js/exercise.js
+++ www/js/exercise.js
@@ -10,15 +10,24 @@ $('#startFixation').click(function() {
 
 $('#startSaccades').click(function() {
   $('#exercise_canvas').addClass('saccades');
+  $('#exercise_canvas').removeClass('fixation');
+  $('#exercise_canvas').removeClass('pursuits');
+  $('#exercise_canvas').removeClass('peripherals');
   $('#enhancements').show();
 });
 
 $('#startPursuits').click(function() {
   $('#exercise_canvas').addClass('pursuits');
+  $('#exercise_canvas').removeClass('fixation');
+  $('#exercise_canvas').removeClass('saccades');
+  $('#exercise_canvas').removeClass('peripherals');
   $('#enhancements').show();
 });
 
 $('#startPeripherals').click(function() {
   $('#exercise_canvas').addClass('peripherals');
+  $('#exercise_canvas').removeClass('pursuits');
+  $('#exercise_canvas').removeClass('fixation');
+  $('#exercise_canvas').removeClass('saccades');
   $('#enhancements').show();
 });
